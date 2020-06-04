f3237f5305d739fd8e98665d14f3bcfff8fe1df5
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-06-04 14:01:19 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
+# this is here because we need to access the Vehicle class
 require_relative "./vehicle.rb"
+
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
